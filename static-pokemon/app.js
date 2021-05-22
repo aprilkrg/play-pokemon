c97c0be289740320b08d8089ee8eb51412f5cedb
aprilkrgonzales <april.gonzales@generalassemb.ly>
2021-05-21 21:01:22 -0600

    leared that I was using some const variables that was stopping my code, as well as using a return statement that stopped my other invocations from working. can play through a hand without comparison, and it won't stop at hand.length = 0

--- static-pokemon/app.js
+++ static-pokemon/app.js
@@ -74,8 +74,8 @@ class Game {
         console.log(`============ \n Today ${this.player.name} will battle ${this.opponent.name}! \n Good luck trainers! \n============`);
     }
     dealHands() {
-        const hand1 = this.cardDeck.splice(0,3);
-        const hand2 = this.cardDeck.splice(0,3);
+        const hand1 = this.cardDeck.splice(0, 3);
+        const hand2 = this.cardDeck.splice(0, 3);
         this.player.hand.push(hand1);
         this.opponent.hand.push(hand2);
     }
@@ -100,57 +100,96 @@ class Player {
     }
     displayHand() {
         console.log(`${this.name}'s pokemon for Round ${game.roundsPlayed + 1} are:`);
-        for(let i = 0; i < this.hand[0].length; i++) {
+        for (let i = 0; i < this.hand[0].length; i++) {
             console.log(`--${this.hand[0][i].name} has ${this.hand[0][i].damage} points of damage`);
         };
     }
+    removeCard() {
+        let names = [];
+            for (let i = 0; i < game.player.hand[0].length; i++) {
+                names.push(` ${i + 1}: ${game.player.hand[0][i].name}(${game.player.hand[0][i].damage} pts)`);
+            };
+        let answer;
+        // console.log(names, 'these are NAMES')
+        setTimeout(function () {
+            let answer = prompt(`${game.player.name}, which pokemon will you choose? \n ${names}`);
+            // console.log(answer, 'answer in pickCard');
+            answer = Number(answer);
+            answer--;
+            // console.log(answer, typeof answer);
+            answer = game.player.hand[0].splice(answer, 1);
+            console.log(answer, typeof answer);
+            console.log(`${JSON.stringify(game.player.hand)}`, 'player hand after pick');
+            if(game.opponent.hand.length > 0) {game.opponent.playCard()};
+            // return answer;
+        }, 500);
+        // const pickedCard = game.player.hand[0].splice(answer, 1);
+        // console.log(`You picked: ${pickedCard}`)
+        // console.log(game.player.hand[0].splice(answer, 1));
+
+        // if(answer !== undefined) {
+        //     console.log(answer, typeof answer, 'outside timeout <<<');
+        // }
+        // console.log("You've chosen:", prompt(`Which pokemon do you choose? \n${names}`));
+        // console.log(answer, typeof answer);
+        // return answer;
+    }
     pickCard() {
         // pickCard gives the player the opportunity to choose their card
-        
-        if(this.hand.length > 0) {
-            let names = [];
-            for(let i = 0; i < this.hand[0].length; i++) {
-                names.push(` ${i+1}: ${this.hand[0][i].name}(${this.hand[0][i].damage} pts)`);
-            };
+        let answer;
+        if (game.player.hand.length > 0) {
+            // let names = [];
+            // for (let i = 0; i < game.player.hand[0].length; i++) {
+            //     names.push(` ${i + 1}: ${game.player.hand[0][i].name}(${game.player.hand[0][i].damage} pts)`);
+            // };
             // console.log(`Pokemon to choose from: ${names}`);
-            let answer;
-            // console.log(names, 'these are NAMES')
-            setTimeout(function(){
-                let answer = prompt(`${game.player.name}, which pokemon will you choose? \n ${names}`);
-                // console.log(answer, 'answer in pickCard');
-                answer = Number(answer);
-                console.log(answer, typeof answer);
-                return answer;
-            }, 500);
-            if(answer !== undefined) {
-                console.log(answer, typeof answer, 'outside timeout <<<');
+            // let answer;
+            // // console.log(names, 'these are NAMES')
+            // setTimeout(function () {
+            //     let answer = prompt(`${game.player.name}, which pokemon will you choose? \n ${names}`);
+            //     // console.log(answer, 'answer in pickCard');
+            //     answer = Number(answer);
+            //     answer--;
+            //     // console.log(answer, typeof answer);
+            //     answer = game.player.hand[0].splice(answer, 1);
+            //     // console.log(answer, typeof answer);
+            //     return answer;
+            // }, 500);
+            // const pickedCard = game.player.hand[0].splice(answer, 1);
+            // // console.log(`You picked: ${pickedCard}`)
+            // console.log(game.player.hand[0].splice(answer, 1));
 
-            }
-            // console.log("You've chosen:", prompt(`Which pokemon do you choose? \n${names}`));
-            // console.log(answer);
+            // // if(answer !== undefined) {
+            // //     console.log(answer, typeof answer, 'outside timeout <<<');
+            // // }
+            // // console.log("You've chosen:", prompt(`Which pokemon do you choose? \n${names}`));
+            // // console.log(answer);
+            // return answer;
+            this.removeCard();
         } else {
             console.log("my hand's empty");
         };
+        // console.log(answer, 'outside the if check')
     }
     playCard() {
-        if(this.hand) {
+        if (game.opponent.hand.length > 0) {
             // playCard is the automatic card selection method for the CPU
             // console.log(`${JSON.stringify(this.hand[0])}`, 'this.hand[0]');
-            const playedCard = game.opponent.hand[0].splice(0, 1);
+            let playedCard = game.opponent.hand[0].splice(0, 1);
             // console.log(`${JSON.stringify(playedCard[0])}`, '<playedCard');
             // console.log(this.hand[0][0]);
             // console.log(game.cardGraveyard, '<graveyard');
             // game.cardGraveyard.push(playedCard[0]);
             // console.log(game.cardGraveyard, '<<graveyard');
-            
+
             // I don't understand why the objects are being accessed like this, but I care about moving forward, so even if it's ugly I'm moving forward
-            const discardCard = playedCard[0];
+            let discardCard = playedCard[0];
             // console.log(discardCard, '<discardCard')
             game.cardGraveyard.push(discardCard);
             // console.log(`${JSON.stringify(game.cardGraveyard)}`, '<<graveyard');
             // console.log(`${JSON.stringify(this.hand[0])}`, 'cpu hand <<');
             console.log(`${game.opponent.name} plays ${JSON.stringify(playedCard[0])}! \n`);
-            setTimeout(game.player.pickCard(), 4000);
+            setTimeout(game.player.removeCard(), 4000);
         };
     }
 };
