82a39303b24e5063c68db53efadc2b5ab6edc83c
aprilkrgonzales <april.gonzales@generalassemb.ly>
2021-05-21 20:34:05 -0600

    changed playCard and pickCard to use the game.opponent and game.player instead of this.hand - because that was causing the user to only have two cards to choose from TL;DR fixed a problem I made

--- static-pokemon/app.js
+++ static-pokemon/app.js
@@ -79,6 +79,9 @@ class Game {
         this.player.hand.push(hand1);
         this.opponent.hand.push(hand2);
     }
+    displayStats() {
+        console.log(`============ \n Round: ${this.roundsPlayed} \n Cards in the deck: ${this.cardDeck.length} \n Discard pile: ${this.cardGraveyard.length}`)
+    }
 };
 
 class Player {
@@ -96,17 +99,59 @@ class Player {
         this.displayHand()
     }
     displayHand() {
-        console.log(`${this.name}'s pokemon for this round are:`)
+        console.log(`${this.name}'s pokemon for Round ${game.roundsPlayed + 1} are:`);
         for(let i = 0; i < this.hand[0].length; i++) {
-            console.log(`--${this.hand[0][i].name} has ${this.hand[0][i].damage} points of damage`)
-        }
+            console.log(`--${this.hand[0][i].name} has ${this.hand[0][i].damage} points of damage`);
+        };
     }
     pickCard() {
-        let names = [];
-        for(let i = 0; i < this.hand[0].length; i++) {
-            names.push(` ${this.hand[0][i].name}: ${this.hand[0][i].damage} pts`)
-        }
-        console.log("You've chosen:", prompt(`Which pokemon do you choose? \n${names}`))
+        // pickCard gives the player the opportunity to choose their card
+        
+        if(this.hand.length > 0) {
+            let names = [];
+            for(let i = 0; i < this.hand[0].length; i++) {
+                names.push(` ${i+1}: ${this.hand[0][i].name}(${this.hand[0][i].damage} pts)`);
+            };
+            // console.log(`Pokemon to choose from: ${names}`);
+            let answer;
+            // console.log(names, 'these are NAMES')
+            setTimeout(function(){
+                let answer = prompt(`${game.player.name}, which pokemon will you choose? \n ${names}`);
+                // console.log(answer, 'answer in pickCard');
+                answer = Number(answer);
+                console.log(answer, typeof answer);
+                return answer;
+            }, 500);
+            if(answer !== undefined) {
+                console.log(answer, typeof answer, 'outside timeout <<<');
+
+            }
+            // console.log("You've chosen:", prompt(`Which pokemon do you choose? \n${names}`));
+            // console.log(answer);
+        } else {
+            console.log("my hand's empty");
+        };
+    }
+    playCard() {
+        if(this.hand) {
+            // playCard is the automatic card selection method for the CPU
+            // console.log(`${JSON.stringify(this.hand[0])}`, 'this.hand[0]');
+            const playedCard = game.opponent.hand[0].splice(0, 1);
+            // console.log(`${JSON.stringify(playedCard[0])}`, '<playedCard');
+            // console.log(this.hand[0][0]);
+            // console.log(game.cardGraveyard, '<graveyard');
+            // game.cardGraveyard.push(playedCard[0]);
+            // console.log(game.cardGraveyard, '<<graveyard');
+            
+            // I don't understand why the objects are being accessed like this, but I care about moving forward, so even if it's ugly I'm moving forward
+            const discardCard = playedCard[0];
+            // console.log(discardCard, '<discardCard')
+            game.cardGraveyard.push(discardCard);
+            // console.log(`${JSON.stringify(game.cardGraveyard)}`, '<<graveyard');
+            // console.log(`${JSON.stringify(this.hand[0])}`, 'cpu hand <<');
+            console.log(`${game.opponent.name} plays ${JSON.stringify(playedCard[0])}! \n`);
+            setTimeout(game.player.pickCard(), 4000);
+        };
     }
 };
 
@@ -118,9 +163,16 @@ const game = new Game(player, cpu);
 
 
 // === ! Game Start ! === //
+console.log("..... game.welcomePlayers .....")
 game.welcomePlayers()
+console.log("..... game.displayStats .....")
+game.displayStats()
+console.log("..... game.dealHands .....")
 game.dealHands()
-// player.displayHand()
-player.sayHello()
-player.pickCard()
-
+player.displayHand()
+// console.log("..... player.sayHello .....")
+// player.sayHello()
+// console.log("..... player.pickCard .....")
+// player.pickCard()
+console.log("..... cpu.playCard .....")
+cpu.playCard()
